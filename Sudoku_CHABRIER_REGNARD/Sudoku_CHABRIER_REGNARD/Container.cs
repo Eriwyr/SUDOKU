a06e1f0e4c28c4ac577011ff88c01d657f6d7b86
Eriwyr <mail@v-chabrier.me>
2016-11-23 14:08:32 +0100

    Add new class

---
+++ Sudoku_CHABRIER_REGNARD/Sudoku_CHABRIER_REGNARD/Container.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_CHABRIER_REGNARD
+{
+    class Container
+    {
+        private HashSet<int> hash;
+
+        public Container()
+        {
+            hash = new HashSet<int>();
+        }
+    }
+}
